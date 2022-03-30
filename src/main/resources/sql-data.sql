a1d5685aa9ff447787ab3ab51a355ebb927e3230
Asher Newbigging <asher.newbigging@outlook.com>
2022-03-30 12:28:12 +0100

    Update SQL schema

--- src/main/resources/sql-data.sql
+++ src/main/resources/sql-data.sql
@@ -1,1 +1,9 @@
-INSERT INTO `ims`.`customers` (`first_name`, `surname`) VALUES ('jordan', 'harrison');
+INSERT INTO `ims`.`customers` (`first_name`, `surname`) VALUES
+	('Jordan', 'Harrison'),
+    ('Asher', 'Newbigging'),
+    ('Jane', 'Smith');
+
+INSERT INTO `ims`.`items` (`name`, `price`) VALUES
+	("Bodyboard", 19.99),
+    ("Surfboard", 24.99),
+    ("Skateboard", 16.99);
--- src/main/resources/sql-schema.sql
+++ src/main/resources/sql-schema.sql
@@ -1,12 +1,17 @@
-drop schema ims;
-
 CREATE SCHEMA IF NOT EXISTS `ims`;
 
 USE `ims` ;
 
 CREATE TABLE IF NOT EXISTS `ims`.`customers` (
-    `id` INT(11) NOT NULL AUTO_INCREMENT,
+    `id` BIGINT NOT NULL AUTO_INCREMENT,
     `first_name` VARCHAR(40) DEFAULT NULL,
     `surname` VARCHAR(40) DEFAULT NULL,
+    PRIMARY KEY (`id`)
+);
+
+CREATE TABLE IF NOT EXISTS `ims`.`items` (
+	`id` BIGINT NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(60) DEFAULT NULL,
+    `price` FLOAT DEFAULT 0.0,
     PRIMARY KEY (`id`)
 );
